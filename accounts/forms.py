9c1e1a3fd42720db5a810c5df4be1c6e5970e249
Madi <madi_gaziz@mail.ru>
2023-08-05 18:18:49 +0600

    Внес изменения в registerform

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,19 +1,69 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-from django.contrib.auth import get_user_model
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import password_validation, get_user_model
+
+from accounts.models import User, Profile
 
 
-class User(AbstractUser):
-    pass
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(
-        get_user_model(),
-        related_name='profile',
-        on_delete=models.CASCADE,
-        verbose_name='User'
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control mb-3'
+            }
+        )
+    )
+    password = forms.CharField(
+        max_length=100,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control mb-3'
+            }
+        )
     )
 
-    avatar = models.ImageField(null=False, blank=False, upload_to='avatars', verbose_name='Avatar')
-    created_at = models.DateTimeField(auto_now_add=True)
+
+class RegisterForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'autocomplete': 'new-password',
+                'class': 'form-control mb-3'
+            }
+        ),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(
+            attrs={
+                'autocomplete': 'new-password',
+                'class': 'form-control mb-3'
+            }
+        ),
+        strip=False,
+        help_text="Enter the same password as before, for verification.",
+    )
+
+    avatar = forms.ImageField(
+        label='Avatars',
+        widget=forms.ClearableFileInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'avatar',
+        })
+    )
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = [
+            'username', 'avatar',
+            'email', 'password1', 'password2'
+        ]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control mb-3'}),
+        }
