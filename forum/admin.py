b540d5464218fe12c8b11dce2844799e01262fc2
Madi <madi_gaziz@mail.ru>
2023-08-05 18:35:21 +0600

    Отобразил в админ панели данные

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from forum.models import Theme, Comment
 # Register your models here.
+
+class ThemeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'count_answer', 'time_of_create']
+
+
+admin.site.register(Theme ,ThemeAdmin)
+admin.site.register(Comment)
